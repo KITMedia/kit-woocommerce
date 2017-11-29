4996cb7eefe71feae1152800e2e017a48a357d59
Christoffer Larsson <christoffer.larsson@kit.se>
2017-11-29 10:30:21 +0100

    Skeleton for further functionality

--- kit-woocommerce.php
+++ kit-woocommerce.php
@@ -7,4 +7,22 @@ Author:       KIT Media
 Author URI:   https://kitmedia.se
 */
 
-error_log( "Hello World" );
+
+add_action( 'init', 'kit_woocommerce_init' );
+
+function kit_woocommerce_init() {
+
+	add_filter( "facebook_for_woocommerce_integration_prepare_product", function ( $product_data, $id ) {
+
+		// Image url (replace with cool version from Imgix)
+		$product_data['image_url'] = $product_data['image_url'] . '?kit-woocommerce';
+
+		// Product url (maybe link directly to Amazon item)
+		$product_data['url'] = $product_data['url'] . '?kit-woocommerce';
+
+		// Checkout url (maybe checkout directly at Amazon)
+		$product_data['checkout_url'] = $product_data['checkout_url'] . '&kit-woocommerce';
+
+		return $product_data;
+	} );
+}
