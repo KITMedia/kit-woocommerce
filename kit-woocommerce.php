6b9a6b6b923c10595eeec154b8f89a79de716fb0
Christoffer Larsson <christoffer.larsson@kit.se>
2017-11-29 09:53:49 +0100

    Adding plugin file

---
+++ kit-woocommerce.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name:  KIT Woocommerce
+Plugin URI:   https://kitmedia.se
+Description:  KIT Woocommerce customizer
+Author:       KIT Media
+Author URI:   https://kitmedia.se
+*/
+
+error_log( "Hello World" );
